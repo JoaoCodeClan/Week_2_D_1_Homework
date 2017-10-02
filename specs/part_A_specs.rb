004585af821cd9da79b0a6802dce423f7a24465a
Joao <johnynecas@hotmail.com>
2017-10-02 16:10:19 +0100

    added first function and test for name

--- specs/part_A_specs.rb
+++ specs/part_A_specs.rb
@@ -3,28 +3,13 @@ require("minitest/rg")
 require_relative("../part_A.rb")
 
 class TestPartA < MiniTest::Test
+  @student
+  def setup
 
+    @student = CodeClanStudent.new("Joao",16)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+  end
+  def test_student_name
+    assert_equal("Joao",@student.name)
+  end
 end
